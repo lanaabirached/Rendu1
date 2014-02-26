4ed656d65d16633c9032fe8ad9d0cb0d56edbe33
lanaabirached <lanaabirached@hotmail.com>
2014-02-26 09:27:50 +0100

    starting rendu1

---
+++ Rendu1/Rendu1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Rendu1
+//
+//  Created by Lana macbook on 26/02/14.
+//  Copyright (c) 2014 Lana macbook. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
